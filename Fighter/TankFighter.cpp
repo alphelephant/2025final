17ec3a074f57b77dfcdae55508f2a45f3decf63b
李昀謙eric <ericycl1118@gmail.com>
2025-06-14 05:41:41 +0800

    大約調完參數了，關卡待設計

--- Fighter/TankFighter.cpp
+++ Fighter/TankFighter.cpp
@@ -6,7 +6,7 @@
 // TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
 TankFighter::TankFighter(float x, float y) : Fighter("play/enemy-4.png", x, y, 
     /*detactradian*/25, /*speed*/150,
-    /*hp*/ 100,
+    /*hp*/ 200,
     /*attack range*/ 150 ,/*damageRange*/150, /*attackspeed*/ 2.0f) {
     damage = 200;
 }
